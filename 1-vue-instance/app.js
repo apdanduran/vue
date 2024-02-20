a18621d8598457d0f925a2800c40799ed0bcf0c1
DURAN APDAN <d.apdan@hotmail.com>
2024-02-20 18:23:06 +0300

    event ,changed coords, this used

--- 1-vue-instance/app.js
+++ 1-vue-instance/app.js
@@ -1,16 +1,32 @@
 const app = Vue.createApp({
-    data(){
-        return{
-            title:"Title of VueJS",
-            content:"Lorem ipsum dolor sit amet",
-            url:"www.duranapdan.com",
-            eduflow:{
-                title:"Sayfamı açmak için tıklayınız.",
-                target:"_blank",
-                url:"duranapdan.com",
-                alt:"duran-apdan"
-            },
-            owner:"Pogaca",
-        }
-    }
+  data() {
+    return {
+      title: "Title of VueJS",
+      content: "Lorem ipsum dolor sit amet",
+      url: "www.duranapdan.com",
+      eduflow: {
+        title: "Sayfamı açmak için tıklayınız.",
+        target: "_blank",
+        url: "duranapdan.com",
+        alt: "duran-apdan",
+      },
+      owner: "Pogaca",
+      coords: {
+        x: 0,
+        y: 0,
+      },
+    };
+  },
+  methods: {
+    changeTitle(pTitle) {
+      this.title = pTitle;
+    },
+    updateCoords(message, event) {
+      this.changeTitle(`${event.x}, ${event.y}`);
+      this.coords = {
+        x: event.x,
+        y: event.y,
+      };
+    },
+  },
 }).mount("#app");
